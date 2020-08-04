818febdac2c59b1ba45db8bc12efa53780302559
eliran <manzeli@me.com>
2020-08-04 14:48:58 +0300

    Updates

---
+++ precache-manifest.6ea97133d9467bcef394592f10fe6372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e113ce307e97c61202c57dac8221a625",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "6e8378bd12d052c29dbd",
+    "url": "/todolist/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "85e6e348f250be2e8c44",
+    "url": "/todolist/static/js/2.2984d714.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.2984d714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e8378bd12d052c29dbd",
+    "url": "/todolist/static/js/main.a8946f2e.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
